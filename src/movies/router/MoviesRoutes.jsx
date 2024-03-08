82806257d4346b370b823351712210e0cbd10fc3
Erick Dejo Vicente <61488294+mapachefurioso@users.noreply.github.com>
2024-03-07 23:21:26 -0500

    Agregado el redirect

--- src/movies/router/MoviesRoutes.jsx
+++ src/movies/router/MoviesRoutes.jsx
@@ -4,8 +4,10 @@ import { CharacterPage, DetailePage, MainPage, SearchPage } from "../pages";
 export const ChildMoviesRoutes = [
   { path: "character/:mal_id", element: <CharacterPage /> },
   { path: "detail/:mal_id", element: <DetailePage /> },
+  
   { path: "main", element: <MainPage/>},
   { path: "search", element: <SearchPage/>},
+
   { path: "/*", element: <Navigate to="/main" /> },
   { path: "/", element: <Navigate to="/main" /> },
 ];
